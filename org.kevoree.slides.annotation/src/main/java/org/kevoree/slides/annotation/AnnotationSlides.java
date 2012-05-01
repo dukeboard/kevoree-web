af263df20c4583190bc40962f579c84cb553f370
Erwan Daubert <Erwan.Daubert@gmail.com>
2012-05-01 13:27:00 +0200

    fix annotation slides

---
+++ org.kevoree.slides.annotation/src/main/java/org/kevoree/slides/annotation/AnnotationSlides.java
@@ -0,0 +1,18 @@
+package org.kevoree.slides.annotation;
+
+import org.kevoree.annotation.ComponentType;
+import org.kevoree.annotation.Library;
+import org.kevoree.slides.template.KevoreeSlidesShowerTemplate;
+
+/**
+ * User: Erwan Daubert - erwan.daubert@gmail.com
+ * Date: 30/04/12
+ * Time: 10:57
+ *
+ * @author Erwan Daubert
+ * @version 1.0
+ */
+@Library(name = "slideshow")
+@ComponentType
+public class AnnotationSlides extends KevoreeSlidesShowerTemplate {
+}
