db1de14030de5adec474405a80d90269047d4fab
barais@irisa.fr <barais@irisa.fr>
2012-05-02 23:43:05 +0200

    last

---
+++ org.kevoree.slides.adam/src/main/java/org/kevoree/slides/adamSlides/AdamSlides.java
@@ -0,0 +1,18 @@
+package org.kevoree.slides.adamSlides;
+
+import org.kevoree.annotation.ComponentType;
+import org.kevoree.annotation.Library;
+import org.kevoree.slides.template.KevoreeSlidesShowerTemplate;
+
+/**
+ * User: Erwan Daubert - erwan.daubert@gmail.com
+ * Date: 30/04/12
+ * Time: 10:57
+ *
+ * @author Erwan Daubert
+ * @version 1.0
+ */
+@Library(name = "slideshow")
+@ComponentType
+public class AdamSlides extends KevoreeSlidesShowerTemplate {
+}
---
+++ org.kevoree.slides.adam/src/main/java/org/kevoree/slides/adamSlides/AdamSlidesDev.java
@@ -0,0 +1,41 @@
+package org.kevoree.slides.adamSlides;
+
+import org.kevoree.annotation.*;
+import org.kevoree.library.javase.webserver.KevoreeHttpRequest;
+import org.kevoree.library.javase.webserver.KevoreeHttpResponse;
+import org.kevoree.slides.template.KevoreeSlidesShowerTemplateDev;
+
+import java.io.File;
+
+/**
+ * User: Erwan Daubert - erwan.daubert@gmail.com
+ * Date: 30/04/12
+ * Time: 10:57
+ *
+ * @author Erwan Daubert
+ * @version 1.0
+ */
+@Library(name = "slideshow")
+@ComponentType
+@DictionaryType({@DictionaryAttribute(name = "folder")})
+public class AdamSlidesDev extends KevoreeSlidesShowerTemplateDev {
+
+	private File devDirectory;
+
+	@Start
+	public void startPage () {
+		super.startPage();
+		File f1 = new File((String) super.getDictionary().get("folder"));
+		if (f1.isDirectory()) {
+			devDirectory = f1;
+		}
+	}
+
+	@Override
+	public KevoreeHttpResponse process (KevoreeHttpRequest request, KevoreeHttpResponse response) {
+		if (!load(request, response, devDirectory.getAbsolutePath())) {
+			super.process(request, response);
+		}
+		return response;
+	}
+}
